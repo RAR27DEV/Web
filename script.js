f3cf364712056ce69379120295045e6287f1926e
RAR27DEV <rafiqanderama1@gmail.com>
2024-08-24 19:44:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    alert('Game is Starting!');
+    window.location.href = 'game.html'; // Redirect ke halaman permainan
+});
