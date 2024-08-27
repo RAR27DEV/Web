90ac28b1239e0517a408092bb73bb40f926c33bb
RAR27DEV <rafiqanderama1@gmail.com>
2024-08-27 13:50:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-document.getElementById('startButton').addEventListener('click', function() {
-    alert('Game is Starting!');
-    window.location.href = 'game.html'; // Redirect ke halaman permainan
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.option-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const buttonText = button.textContent.trim().toLowerCase();
+            switch(buttonText) {
+                case 'story':
+                    window.location.href = 'substory.html';
+                    break;
+                case 'kuis':
+                    window.location.href = 'game.html';
+                    break;
+                case 'puzzle':
+                    window.location.href = 'starpuzzle.html';
+                    break;
+                case 'coming soon':
+                    alert('Developer masih nyari ide jadi sabarya, kalau mau cepat kasi ide aja lansunng ke developernya');
+                    break;
+                default:
+                    alert('Halaman tidak tersedia');
+            }
+        });
+    });
 });
